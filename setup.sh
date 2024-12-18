503ff0e92bbd0a6e41e145f4b18c4d20a64c3fa4
Braden Mitchell <32005299+bradenmitchell@users.noreply.github.com>
2024-12-18 23:20:59 +1030

    fix(setup): fix xcode CLT install in setup script

--- setup.sh
+++ setup.sh
@@ -7,11 +7,11 @@ DOTFILES_GIT_REMOTE="https://github.com/bradenmitchell/dotfiles.git"
 # Check if Xcode Command Line Tools installed
 echo "Checking for Xcode Command Line Tools..."
 if ! xcode-select -p &>/dev/null; then
-  echo "Error: Xcode Command Line Tools not found"
-  echo "Use \`xcode-select --install\` to install then re-run this script"
+  echo "Error: Xcode Command Line Tools not found."
+  echo "Use \`xcode-select --install\` to install manually and re-run this script."
   exit 1
 else
-  echo "Xcode Command Line Tools available"
+  echo "Xcode Command Line Tools available."
 fi
 
 # Check if dotfiles repository exists
@@ -25,14 +25,14 @@ fi
 cd $DOTFILES_DIR
 
 # Check if homebrew installed
-echo "Checking for homebrew"
+echo "Checking for homebrew..."
 if ! command -v brew &>/dev/null; then
   # Install homebrew using non-interactive install
-  echo "Installing homebrew"
+  echo "Installing homebrew..."
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   # Add homebrew to PATH for this session
-  echo "Configuring PATH for current session"
+  echo "Configuring PATH for current session..."
   case "$(uname -m)" in
     arm64)  # Apple Silicon Mac
       echo "Homebrew PATH configured for Apple Silicon (arm64)."
@@ -47,6 +47,9 @@ if ! command -v brew &>/dev/null; then
       exit 1
       ;;
   esac
+else
+  echo "Homebrew available."
+fi
 
 # Check that homebrew is now accessible in current session
 if ! command -v brew &>/dev/null; then
