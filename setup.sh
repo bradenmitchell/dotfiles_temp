a18e86ce88b0cf6f5317548e7cf890129d47c2e3
Braden Mitchell <32005299+bradenmitchell@users.noreply.github.com>
2024-12-18 22:54:33 +1030

    fix(setup): fix xcode CLT install in setup script

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ DOTFILES_DIR="${HOME}/.dotfiles"
 DOTFILES_GIT_REMOTE="https://github.com/bradenmitchell/dotfiles.git"
 
 # Check if Xcode Command Line Tools installed
-echo "Checking if Xcode Command Line Tools are installed..."
+echo "Checking for Xcode Command Line Tools..."
 if ! xcode-select -p &>/dev/null; then
   echo "Error: Xcode Command Line Tools not found"
-  echo "run \`xcode-select --install\` then re-run this script"
+  echo "Use \`xcode-select --install\` to install then re-run this script"
   exit 1
 else
   echo "Xcode Command Line Tools available"
@@ -21,38 +21,38 @@ if [[ ! -d "$DOTFILES_DIR" ]]; then
   git clone $DOTFILES_GIT_REMOTE $DOTFILES_DIR
 fi
 
-# # Change to dotfiles directory
-# cd $DOTFILES_DIR
+# Change to dotfiles directory
+cd $DOTFILES_DIR
 
-# # Check if homebrew installed
-# echo "Checking for homebrew"
-# if ! command -v brew &>/dev/null; then
-#   # Install homebrew using non-interactive install
-#   echo "Installing homebrew"
-#   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Check if homebrew installed
+echo "Checking for homebrew"
+if ! command -v brew &>/dev/null; then
+  # Install homebrew using non-interactive install
+  echo "Installing homebrew"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-#   # Add homebrew to PATH for this session
-#   echo "Configuring PATH for current session"
-#   case "$(uname -m)" in
-#     arm64)  # Apple Silicon Mac
-#       echo "Homebrew PATH configured for Apple Silicon (arm64)."
-#       export PATH="/opt/homebrew/bin:$PATH"
-#       ;;
-#     x86_64)  # Intel-based Mac
-#       echo "Homebrew PATH configured for Intel (x86_64)."
-#       export PATH="/usr/local/bin:$PATH"
-#       ;;
-#     *)  # Unknown architecture
-#       echo "Error: Unknown system architecture: $(uname -m). PATH not configured."
-#       exit 1
-#       ;;
-#   esac
+  # Add homebrew to PATH for this session
+  echo "Configuring PATH for current session"
+  case "$(uname -m)" in
+    arm64)  # Apple Silicon Mac
+      echo "Homebrew PATH configured for Apple Silicon (arm64)."
+      export PATH="/opt/homebrew/bin:$PATH"
+      ;;
+    x86_64)  # Intel-based Mac
+      echo "Homebrew PATH configured for Intel (x86_64)."
+      export PATH="/usr/local/bin:$PATH"
+      ;;
+    *)  # Unknown architecture
+      echo "Error: Unknown system architecture: $(uname -m). PATH not configured."
+      exit 1
+      ;;
+  esac
 
-# # Check that homebrew is now accessible in current session
-# if ! command -v brew &>/dev/null; then
-#   echo "Error: Unable to locate homebrew in PATH."
-#   exit 1
-# fi
+# Check that homebrew is now accessible in current session
+if ! command -v brew &>/dev/null; then
+  echo "Error: Unable to locate homebrew in PATH."
+  exit 1
+fi
 
 # # Install packages, applications and fonts
 # echo "Installing packges and applications via homebrew"
